7cee73ca7cb7cbe24cf7d7d188e0a7dc94a2b8a9
junkingboy <junkingboy@163.com>
2024-04-08 21:21:35 +0800

    This is first time to push to origin

---
+++ src/AllChangeNumberFinal.cpp
@@ -0,0 +1,12 @@
+﻿#include "numberImp.hpp"
+#include "numberFun.hpp"
+
+int main()
+{
+    map<string, string> data = NumberFunction::get_info_from_input("101010.110", "8");
+    Number num(data["decimal"], data["part"], data["symbol"], data["base"]);
+    string val = num.ret_data();
+    string binaryStr = num.binary_to_hex(val, "decimal");
+    cout << "Decimal number change to binary number of ->" << val << "\t:\t" << binaryStr << endl;
+    return 0;
+}
